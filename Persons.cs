0d96a2d25a29ded9b718e040f4bd3b9cb97517a8
Gabriel Uifalean <gabriel13ro@gmail.com>
2024-10-17 10:03:50 +0200

    new project

--- Persons.cs
+++ Persons.cs
@@ -10,5 +10,65 @@ namespace TjuvOchPolis_4
     {
         public int X {  get; set; }
         public int Y { get; set; }
+        private static Random random = new Random();
+
+        public Persons(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Move(int width,int heigth)// method
+        {
+            X += random.Next(-1, 2);
+            Y += random.Next(-1, 2);
+
+            // om en person ga ut kommer hen tillbaka I andra ändan
+            if (X < 0) X = width -1;// vanster - hoger
+            if (X >= width) X = 0; // hoger - vanster
+                           
+            if (Y < 0) Y = heigth - 1; // upp- ner
+            if (Y >= heigth) Y = 0; // ner-upp 
+
+
+        }
+        public class Citizen : Persons  // Citizen med arv fran Persons
+        {
+            public List<string> Inventory { get; set; }
+
+            public Citizen(int x, int y) : base(x, y) // constructor
+            {
+                Inventory = new List<string>() { "Nycklar ", "Mobiltelefon ", "Pengar ", "Klocka" };
+            }
+
+        }
+        public class Thief : Persons
+        {
+            public List <string> ThiefInventory { get; set; }
+            public Thief(int x , int y) : base(x, y)
+            {
+                ThiefInventory = new List<string>() { };
+            }
+            public void Steal(Citizen citizen) // tjuv method
+            {
+                if (citizen.Inventory.Count > 0) 
+                {
+                    Random random = new Random(); // tjuven tar en random item
+                    int index = random.Next(citizen.Inventory.Count);
+                    string stolenItem = citizen.Inventory[index];
+
+
+                }
+            }
+        }
+        public class Police : Persons
+        {
+            public List<string> PoliceInventory { get; set; }
+
+            public Police(int x, int y) : base(x, y)
+            {
+                PoliceInventory = new List<string>();
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace TjuvOchPolis_4
+﻿using System.IO.Pipes;
+
+namespace TjuvOchPolis_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int width = 100;
+            int height = 25;
+
+            //test temporal
+
+            Persons p1 = new Persons(50,3);
+            Persons p2 = new Persons(66,5);
+            Persons p3 = new Persons(2,12);
+            Console.WriteLine($"{ p1.X},{ p1.Y}");
+            Console.WriteLine($"{p3.X},{p3.Y}");
+            Console.WriteLine($"{p2.X},{p2.Y}");
+
+            p1.Move(width,height);
+            p2.Move(width,height);
+            p3.Move(width,height);
+
+
+            // test temporal
+            Console.WriteLine($"Persoana1 se misca :{p1.X } , {p1.Y}");
+            Console.WriteLine($"Persoana 2 se misca : {p2.X}, {p2.Y}");
         }
     }
 }
