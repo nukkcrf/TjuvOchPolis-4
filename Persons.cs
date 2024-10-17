4b212e1499cf9fce48c94c35b1bae6ef4a597d84
Gabriel Uifalean <gabriel13ro@gmail.com>
2024-10-17 08:27:51 +0200

    Add project files.

---
+++ Persons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolis_4
+{
+    public class Persons
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+    }
+}
